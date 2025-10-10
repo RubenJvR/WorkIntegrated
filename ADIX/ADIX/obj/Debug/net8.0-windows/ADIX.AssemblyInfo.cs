6f0defb8091da93949df097c07def7f2c2d20a2d
44KAbb <107203049+44KAbb@users.noreply.github.com>
2025-10-10 11:58:40 +0200

    Minor fix to POS pageUI

--- ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
+++ ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6393108662bc176e514d7a55e69a689b26bb313")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182f2b47906f3bff8de90b8e3f3a7606030a150e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
