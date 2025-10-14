1e45a6ba278d426d6b8e933cdeb3732d17173ad3
44KAbb <107203049+44KAbb@users.noreply.github.com>
2025-10-15 01:01:14 +0200

    Added Product Front end only slight change to the backend (Table purposes)

--- ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
+++ ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f0defb8091da93949df097c07def7f2c2d20a2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b80f05ebc6c0834189d1e20a7a3fb53c497fc01")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
