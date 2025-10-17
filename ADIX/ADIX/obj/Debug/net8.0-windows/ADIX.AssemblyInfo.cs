70680594ad57d87108b488c8f0597ee4cb2baf4f
44KAbb <107203049+44KAbb@users.noreply.github.com>
2025-10-17 11:55:58 +0200

    Invoice and Qoute UI added (Not looking at its best yet)

--- ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
+++ ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e45a6ba278d426d6b8e933cdeb3732d17173ad3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af582d314d8a04da3c40154ea2b661b06bbd95a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
