6a9b38e1cfc8b3901dde81d49462b3e966620bf4
44KAbb <107203049+44KAbb@users.noreply.github.com>
2025-10-20 12:05:29 +0200

    Unnecessary Changes

--- ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
+++ ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f406f5d6fb21dddea8a99ca9d5bc7fd69078c153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3f21392f62510191ed94406c2038aecac2dc76")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
