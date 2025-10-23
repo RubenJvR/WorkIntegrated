7d882f4abad01f21edb38a1f0061251daed791d1
Ruben Janse van Rensburg <60285670+RubenJvR@users.noreply.github.com>
2025-10-23 20:07:47 +0200

    changes

--- ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
+++ ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9abb569260dfd0bbb39a44641a305eb1b3416637")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf8365c0d2f5541d935ee8196f0d554e81883e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
