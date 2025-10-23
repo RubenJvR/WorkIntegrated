4e873807dcdf99392f031e730984f701ec1e7e2e
Tshiamo Mokoena <st10291971@imconnect.edu.za>
2025-10-23 16:36:37 +0200

    Added display items from database feature (ItemID as SKU still needs implementation)

---
+++ ADIX/ADIX/Models/InventoryItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADIX
+{
+    class InventoryItem
+    {
+        public string ItemGroup {  get; set; }
+        public string ItemName { get; set; }
+        public string SKU { get; set; }
+        public int OpeningStockQuantity { get; set;}
+        public int StockReceived { get; set;}
+        public int StockSold { get; set;}
+        public int BalanceStock { get; set; }
+        public int StockReturned { get; set; }
+        public int StockRefunded { get; set; }
+        public double CostOfBusinessWorkings { get; set; }
+        public int ReturnedStockUnusable { get; set; }
+        public double Loss { get; set; }
+
+    }
+}
