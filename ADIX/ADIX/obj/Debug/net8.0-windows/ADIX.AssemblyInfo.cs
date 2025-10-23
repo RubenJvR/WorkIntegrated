ecc3583892191ed417a5188151cab2a853967652
44KAbb <107203049+44KAbb@users.noreply.github.com>
2025-10-23 17:19:12 +0200

    Inventory Table has columns from Itemgroup products
    MonthlyReport changes

--- ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
+++ ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efcb4cc178d3ad28c58d1c6101f520444b1a9767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5223601c94720eb56905d9534c503c397d06844")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
