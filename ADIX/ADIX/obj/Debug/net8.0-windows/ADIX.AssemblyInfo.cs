30c888df2559cf92a6582cd1a8f416806364acae
Ruben Janse van Rensburg <60285670+RubenJvR@users.noreply.github.com>
2025-10-24 14:19:50 +0200

    Merge

--- ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
+++ ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266194324a7516ee9f71a5197b8352c7c4a057bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ac71be88cce220bd1c40e08f0731c5340b81f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
