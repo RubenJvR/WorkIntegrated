2fb9606c26cd4fc63f66e17838c07cfbf30b71bb
Ruben Janse van Rensburg <60285670+RubenJvR@users.noreply.github.com>
2025-10-27 13:59:16 +0200

    removed drops

--- ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
+++ ADIX/ADIX/obj/Debug/net8.0-windows/ADIX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3683458274c8319d41a8941ab8cb9f5dd97afa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3db332a9ccca3667d2abcc0ffd28f6efab6f8f3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADIX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
